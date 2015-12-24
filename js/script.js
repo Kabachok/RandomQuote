4c4d9d527befed39997e35f6b7f8ec9ca05bf2a0
Daria <koumori-daria@mail.ru>
2015-12-24 19:03:21 +0400

    task description
    added TODO elements, implemented basic structure in html file.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var quoteCollection = [
+						"first quote ///author of the quote", 
+						"second quote ///author of the quote"
+					  ];
+
+// TODO: create a random number randomIndex
+
+// TODO: select an item from the quoteCollection at the randomIndex position
+
+// TODO: send choosen item to the html document
